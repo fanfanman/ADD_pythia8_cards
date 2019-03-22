63dd4f632743bc0d830d8eb428ffafff86af5415
Fan <aprilzhang1994@gmail.com>
2019-03-21 23:33:02 -0500

    change 2017 to CUEP8 + add 2018 samples

---
+++ ADD2018/ADDGravToLL_LambdaT-12000_M-1300To2000_13TeV-pythia8_cff.py
@@ -0,0 +1,33 @@
+
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.Pythia8CUEP8M1Settings_cfi import *
+from Configuration.Generator.PSweightsPythia.PythiaPSweightsSettings_cfi import *
+
+generator = cms.EDFilter("Pythia8GeneratorFilter",
+    maxEventsToPrint = cms.untracked.int32(1),
+    pythiaPylistVerbosity = cms.untracked.int32(1),
+    filterEfficiency = cms.untracked.double(1.0),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    comEnergy = cms.double(13000.),
+    PythiaParameters = cms.PSet(
+        pythia8CommonSettingsBlock,
+        pythia8CUEP8M1SettingsBlock,
+        pythia8PSweightsSettingsBlock,
+        processParameters = cms.vstring(
+            'ExtraDimensionsLED:LambdaT = 12000',
+            'ExtraDimensionsLED:ffbar2llbar = on',
+            'ExtraDimensionsLED:gg2llbar = on',
+            'ExtraDimensionsLED:NegInt = 0',
+            'ExtraDimensionsLED:CutOffmode = 0',
+            'PhaseSpace:mHatMin = 1300',
+            'PhaseSpace:mHatMax = 2000',
+        ),
+        parameterSets = cms.vstring('pythia8CommonSettings',
+                                    'pythia8CUEP8M1Settings',
+                                    'pythia8PSweightsSettings',
+                                    'processParameters',
+                                    )
+    )
+)
